84bfd1fd243e40bf2d6910ffd23b73ef6885809c
Seth Phinehas <sethphiny@gmail.com>
2026-04-27 16:14:47 +0100

    chore: untrack backend dist folder and update gitignore

--- backend/dist/database/schemas/base.schema.d.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEntity as TypeOrmBaseEntity } from 'typeorm';
-export declare abstract class BaseEntity extends TypeOrmBaseEntity {
-    id: string;
-    createdOn: Date;
-    updatedOn: Date;
-}
